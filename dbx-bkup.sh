f09c414b006cfa829ab5301e6c4cbc6dd72f71b6
Vimal Atreya Ramaka <var@users.noreply.github.com>
2015-09-10 01:12:00 -0300

    update script path

--- dbx-bkup.sh
+++ dbx-bkup.sh
@@ -1,4 +1,4 @@
 now=`date +"%d-%m-%y-%T"`;
 sudo zip -r stationname-$now /etc/weewx/* /var/lib/weewx/* /var/www/weewx/* /var/log/syslog
-sudo ./dropbox_uploader.sh upload stationname-$now.zip DROPBOXSTATIONFOLDER/stationname-$now.zip
+sudo /home/pi/Dropbox-Uploader/dropbox_uploader.sh upload stationname-$now.zip DROPBOXSTATIONFOLDER/stationname-$now.zip
 sudo rm *.zip
